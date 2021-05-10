4f35e9cebbb6ac21da1014e46aedc36658de63aa
Asel-Ism <asel2468@mail.ru>
2021-05-10 18:50:50 +0600

    Initial commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Calc().setVisible(true);
+// TODO code application logic here
+    }
+    
+}
